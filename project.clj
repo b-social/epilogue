2d9862641a3622beff62aaf7e9db7c1621b76529
Alex Vear <alex.vear@kroo.com>
2023-04-07 18:53:02 +0100

    Don't include Clojure in the compiled Jar

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :url "https://github.com/b-social/epilogue"
   :description "Simple Clojure logging facade for logging structured data via SLF4J 2."
   :pedantic? true
-  :dependencies [[org.clojure/clojure "1.11.1"]
+  :dependencies [[org.clojure/clojure "1.11.1" :scope "provided"]
                  [org.slf4j/slf4j-api "2.0.7"]]
   :profiles {:dev {:dependencies [[org.slf4j/slf4j-simple "2.0.7"]]}})
