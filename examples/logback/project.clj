32184e2a5b3d23ea1ec7a00ff1714f0dad3a036b
Alex Vear <alex.vear@kroo.com>
2023-06-10 19:49:02 +0100

    Add JVM opts for clojure.tools.logging to example project

--- examples/logback/project.clj
+++ examples/logback/project.clj
@@ -7,4 +7,6 @@
                  ;; Add SLF4J logging bridges to make logs from other logging libraries go via SLF4J.
                  [org.slf4j/jul-to-slf4j         "2.0.7"]
                  [org.slf4j/jcl-over-slf4j       "2.0.7"]
-                 [org.slf4j/log4j-over-slf4j     "2.0.7"]])
+                 [org.slf4j/log4j-over-slf4j     "2.0.7"]]
+  ;; Make clojure.tools.logging use SLF4J.
+  :jvm-opts ["-Dclojure.tools.logging.factory=clojure.tools.logging.impl/slf4j-factory"])
