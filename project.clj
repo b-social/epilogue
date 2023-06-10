edd6f818a058f5f627bd66642110496147954726
Alex Vear <alex.vear@kroo.com>
2023-06-10 14:42:01 +0100

    Add Jar compilation options and update `.gitignore`

--- project.clj
+++ project.clj
@@ -5,6 +5,9 @@
             :url "https://github.com/b-social/epilogue/blob/master/LICENCE"
             :distribution :repo}
   :pedantic? true
+  :jvm-opts ["-Dclojure.compiler.direct-linking=true"]
+  :global-vars {*warn-on-reflection* true
+                *assert*             false}
   :dependencies [[org.clojure/clojure "1.11.1" :scope "provided"]
                  [org.slf4j/slf4j-api "2.0.7"]]
   :profiles {:dev {:dependencies [[org.slf4j/slf4j-simple "2.0.7"]]}})
