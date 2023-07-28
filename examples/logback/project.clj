ce2da8f5f0e04425bb184f9f22734eece0dd0583
Alex Vear <alex.vear@kroo.com>
2023-07-28 15:38:51 +0100

    Update examples to use latest versions of Epilogue and Typeset.logback

--- examples/logback/project.clj
+++ examples/logback/project.clj
@@ -1,9 +1,9 @@
 (defproject com.example/logging "1.0"
   :dependencies [[org.clojure/clojure            "1.11.1"]
-                 [com.kroo/epilogue              "0.1"]
+                 [com.kroo/epilogue              "0.2"]
                  ;; Use Logback-classic as the logging backend.
-                 [ch.qos.logback/logback-classic "1.4.7"]
-                 [com.kroo/typeset.logback       "0.1"]
+                 [ch.qos.logback/logback-classic "1.4.8"]
+                 [com.kroo/typeset.logback       "0.2"]
                  ;; Add SLF4J logging bridges to make logs from other logging libraries go via SLF4J.
                  [org.slf4j/jul-to-slf4j         "2.0.7"]
                  [org.slf4j/jcl-over-slf4j       "2.0.7"]
