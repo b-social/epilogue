11fa2e17fb9c8a6c7701ec6dd8095bc7fabd50ff
Alex Vear <alex.vear@kroo.com>
2024-03-13 20:28:05 +0000

    Update dependencies and prep for v0.5 release

--- examples/logback/project.clj
+++ examples/logback/project.clj
@@ -1,12 +1,12 @@
 (defproject com.example/logging "1.0"
-  :dependencies [[org.clojure/clojure            "1.11.1"]
-                 [com.kroo/epilogue              "0.4"]
+  :dependencies [[org.clojure/clojure            "1.11.2"]
+                 [com.kroo/epilogue              "0.5"]
                  ;; Use Logback-classic as the logging backend.
-                 [ch.qos.logback/logback-classic "1.4.14"]
-                 [com.kroo/typeset.logback       "0.3"]
+                 [ch.qos.logback/logback-classic "1.5.3"]
+                 [com.kroo/typeset.logback       "0.4"]
                  ;; Add SLF4J logging bridges to make logs from other logging libraries go via SLF4J.
-                 [org.slf4j/jul-to-slf4j         "2.0.11"]
-                 [org.slf4j/jcl-over-slf4j       "2.0.11"]
-                 [org.slf4j/log4j-over-slf4j     "2.0.11"]]
+                 [org.slf4j/jul-to-slf4j         "2.0.12"]
+                 [org.slf4j/jcl-over-slf4j       "2.0.12"]
+                 [org.slf4j/log4j-over-slf4j     "2.0.12"]]
   ;; Make clojure.tools.logging use SLF4J.
   :jvm-opts ["-Dclojure.tools.logging.factory=clojure.tools.logging.impl/slf4j-factory"])
